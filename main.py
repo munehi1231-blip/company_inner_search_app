3776e0947a3292bc07c15a78aeaa79e82934ba79
munehi1231 <munehi1231@gmail.com>
2026-01-31 22:51:07 +0900

    first commit.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import constants as ct
 ############################################################
 # ブラウザタブの表示文言を設定
 st.set_page_config(
-    page_title=ct.APP_NAME
+    page_title=ct.APP_NAME,
     layout="wide" 
 )
 
