a5c3c505c32a0fc2c844e1646008af9d0ddbc125
munehi1231 <munehi1231@gmail.com>
2026-02-02 12:56:37 +0900

    first commit.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import logging
 import streamlit as st
 # ブラウザタブの表示文言を設定
 st.set_page_config(
-    page_title="Company Inner Search,
+    page_title="Company Inner Search",
     layout="wide" 
 )
 
